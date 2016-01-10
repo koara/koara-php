bf62aad1803ee835a0c153050ccbb6d25a215e52
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-01-10 18:19:23 +0100

    add unicode tests

--- tests/CharStreamTest.php
+++ tests/CharStreamTest.php
@@ -7,11 +7,8 @@ class StringReaderTest extends \PHPUnit_Framework_TestCase {
 
  	private $cs;
 	
-	protected function setUp() {
-		$this->cs = new CharStream(new StringReader("abcd"));
-	}
-	
 	public function testBeginToken() {
+		$this->cs = new CharStream(new StringReader("abcd"));
 		$this->assertEquals('a', $this->cs->beginToken());
 		$this->assertEquals(1, $this->cs->getBeginColumn());
 		$this->assertEquals(1, $this->cs->getBeginLine());
@@ -20,6 +17,7 @@ class StringReaderTest extends \PHPUnit_Framework_TestCase {
 	}
 	
 	public function testReadChar() {
+		$this->cs = new CharStream(new StringReader("abcd"));
 		$this->assertEquals('a', $this->cs->readChar());
 		$this->assertEquals('b', $this->cs->readChar());
 		$this->assertEquals('c', $this->cs->readChar());
@@ -30,6 +28,7 @@ class StringReaderTest extends \PHPUnit_Framework_TestCase {
 	 * @expectedException Exception
 	 */
 	public function testReadCharTillEof() {
+		$this->cs = new CharStream(new StringReader("abcd"));
 		$this->cs->readChar();
 		$this->cs->readChar();
 		$this->cs->readChar();
@@ -38,9 +37,46 @@ class StringReaderTest extends \PHPUnit_Framework_TestCase {
 	}
 	
 	public function testGetImage() {
+		$this->cs = new CharStream(new StringReader("abcd"));
 		$this->cs->readChar();
 		$this->cs->readChar();
 		$this->assertEquals("ab", $this->cs->getImage());
+	}
+	
+	public function testBeginTokenWithUnicode() {
+		$this->cs = new CharStream(new StringReader("ðinæ"));
+		$this->assertEquals('ð', $this->cs->beginToken());
+		$this->assertEquals(1, $this->cs->getBeginColumn());
+		$this->assertEquals(1, $this->cs->getBeginLine());
+		$this->assertEquals(1, $this->cs->getEndColumn());
+		$this->assertEquals(1, $this->cs->getEndColumn());
+	}
+	
+	public function testReadCharWithUnicode() {
+		$this->cs = new CharStream(new StringReader("ðinæ"));
+		$this->assertEquals('ð', $this->cs->readChar());
+		$this->assertEquals('i', $this->cs->readChar());
+		$this->assertEquals('n', $this->cs->readChar());
+		$this->assertEquals('æ', $this->cs->readChar());
+	}
+	
+	/**
+	 * @expectedException Exception
+	 */
+	public function testReadCharTillEofWithUnicode() {
+		$this->cs = new CharStream(new StringReader("ðinæ"));
+		$this->cs->readChar();
+		$this->cs->readChar();
+		$this->cs->readChar();
+		$this->cs->readChar();
+		$this->cs->readChar();
+	}
+	
+	public function testGetImageWithUnicode() {
+		$this->cs = new CharStream(new StringReader("ðinæ"));
+		$this->cs->readChar();
+		$this->cs->readChar();
+		$this->assertEquals("ði", $this->cs->getImage());
 	}
 	
 }
