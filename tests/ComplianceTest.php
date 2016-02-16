8064ba5d3b79e36ecceb8558b596b6474dc70a89
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-02-16 07:11:36 +0100

    add xml renderer

--- tests/ComplianceTest.php
+++ tests/ComplianceTest.php
@@ -2,21 +2,22 @@
 namespace Koara;
 
 use Koara\Renderer\Html5Renderer;
+use Koara\Renderer\XmlRenderer;
 
 class ComplianceTest extends \PHPUnit_Framework_TestCase {
 
 	public function provider() {
 		$i=0;
-		$modules = scandir(dirname(__FILE__).'/testsuite');
+		$modules = scandir(dirname(__FILE__).'/testsuite/input');
 		foreach ($modules as $module) {
-			if($module != '.' && $module != '..' && substr($module, 0, 1) != '_') {
-				$testcases = scandir(dirname(__FILE__).'/testsuite/'.$module.'/koara');
+			if($module != '.' && $module != '..' && $module != 'end2end.kd') {
+				$testcases = scandir(dirname(__FILE__).'/testsuite/input/'.$module);
 				foreach ($testcases as $testcase) {
-					if(substr($testcase, -3) == '.kd') {
+					if($testcase != '.' && $testcase != '..') {
 						$array[$i][0] = $module;
 						$array[$i][1] = substr($testcase, 0, -3);
+						$i++;
 					}
-					$i++;
 				}
 			}
 		}
@@ -26,17 +27,33 @@ class ComplianceTest extends \PHPUnit_Framework_TestCase {
 	/**
 	 * @dataProvider provider
 	 */
-	public function testAdd($module, $testcase) {		
-			$html = file_get_contents(dirname(__FILE__).'/testsuite/'.$module.'/html5/'.$testcase.'.htm');
+	public function testKoaraToHtml5($module, $testcase) {		
+			$html = file_get_contents(dirname(__FILE__).'/testsuite/output/html5/'.$module.'/'.$testcase.'.htm');
 	  		$html = mb_convert_encoding($html, 'UTF-8', mb_detect_encoding($html, 'UTF-8, ISO-8859-1', true));
 		 
 	  		$parser = new Parser();
-	  		$document = $parser->parseFile(dirname(__FILE__).'/testsuite/'.$module.'/koara/'.$testcase.'.kd');
+	  		$document = $parser->parseFile(dirname(__FILE__).'/testsuite/input/'.$module.'/'.$testcase.'.kd');
  		
 	 		$renderer = new Html5Renderer();
 	 		$document->accept($renderer);
 	 		
 	 		$this->assertEquals($html, $renderer->getOutput());
+	}
+	
+	/**
+	 * @dataProvider provider
+	 */
+	public function testKoaraToXml($module, $testcase) {
+		$xml = file_get_contents(dirname(__FILE__).'/testsuite/output/xml/'.$module.'/'.$testcase.'.xml');
+		$xml = mb_convert_encoding($html, 'UTF-8', mb_detect_encoding($html, 'UTF-8, ISO-8859-1', true));
+			
+		$parser = new Parser();
+		$document = $parser->parseFile(dirname(__FILE__).'/testsuite/input/'.$module.'/'.$testcase.'.kd');
+			
+		$renderer = new XmlRenderer();
+		$document->accept($renderer);
+	
+		$this->assertEquals($xml, $renderer->getOutput());
 	}
 
 }
