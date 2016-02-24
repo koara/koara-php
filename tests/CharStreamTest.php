bd18ac5ef61e99bc967adac0bab305232a14cf49
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-02-24 12:38:42 +0100

    initial setup

---
+++ tests/CharStreamTest.php
@@ -0,0 +1,82 @@
+<?php
+namespace Koara;
+
+use Koara\Io\StringReader;
+
+class CharStreamTest extends \PHPUnit_Framework_TestCase {
+
+ 	private $cs;
+	
+	public function testBeginToken() {
+		$this->cs = new CharStream(new StringReader('abcd'));
+		$this->assertEquals('a', $this->cs->beginToken());
+		$this->assertEquals(1, $this->cs->getBeginColumn());
+		$this->assertEquals(1, $this->cs->getBeginLine());
+		$this->assertEquals(1, $this->cs->getEndColumn());
+		$this->assertEquals(1, $this->cs->getEndColumn());
+	}
+	
+	public function testReadChar() {
+		$this->cs = new CharStream(new StringReader('abcd'));
+		$this->assertEquals('a', $this->cs->readChar());
+		$this->assertEquals('b', $this->cs->readChar());
+		$this->assertEquals('c', $this->cs->readChar());
+		$this->assertEquals('d', $this->cs->readChar());
+	}
+	
+	/**
+	 * @expectedException Exception
+	 */
+	public function testReadCharTillEof() {
+		$this->cs = new CharStream(new StringReader('abcd'));
+		$this->cs->readChar();
+		$this->cs->readChar();
+		$this->cs->readChar();
+		$this->cs->readChar();
+		$this->cs->readChar();
+	}
+	
+	public function testGetImage() {
+		$this->cs = new CharStream(new StringReader('abcd'));
+		$this->cs->readChar();
+		$this->cs->readChar();
+		$this->assertEquals("ab", $this->cs->getImage());
+	}
+	
+	public function testBeginTokenWithUnicode() {
+		$this->cs = new CharStream(new StringReader('ðinæ'));
+		$this->assertEquals('ð', $this->cs->beginToken());
+		$this->assertEquals(1, $this->cs->getBeginColumn());
+		$this->assertEquals(1, $this->cs->getBeginLine());
+		$this->assertEquals(1, $this->cs->getEndColumn());
+		$this->assertEquals(1, $this->cs->getEndColumn());
+	}
+	
+	public function testReadCharWithUnicode() {
+		$this->cs = new CharStream(new StringReader('ðinæ'));
+		$this->assertEquals('ð', $this->cs->readChar());
+		$this->assertEquals('i', $this->cs->readChar());
+		$this->assertEquals('n', $this->cs->readChar());
+		$this->assertEquals('æ', $this->cs->readChar());
+	}
+	
+	/**
+	 * @expectedException Exception
+	 */
+	public function testReadCharTillEofWithUnicode() {
+		$this->cs = new CharStream(new StringReader('ðinæ'));
+		$this->cs->readChar();
+		$this->cs->readChar();
+		$this->cs->readChar();
+		$this->cs->readChar();
+		$this->cs->readChar();
+	}
+	
+	public function testGetImageWithUnicode() {
+		$this->cs = new CharStream(new StringReader('ðinæ'));
+		$this->cs->readChar();
+		$this->cs->readChar();
+		$this->assertEquals('ði', $this->cs->getImage());
+	}
+	
+}
---
+++ tests/StringReaderTest.php
@@ -0,0 +1,112 @@
+<?php
+namespace Koara;
+
+use Koara\Io\StringReader;
+
+class StringReaderTest extends \PHPUnit_Framework_TestCase {
+
+ 	private $buffer;
+	
+	protected function setUp() {
+		$this->buffer = [];
+	}
+	
+  	public function testRead() {
+ 		$reader = new StringReader('abcd');
+ 		$this->assertEquals(4, $reader->read($this->buffer, 0, 4));
+ 		$this->assertEquals('a', $this->buffer[0]);
+		$this->assertEquals('b', $this->buffer[1]);
+		$this->assertEquals('c', $this->buffer[2]);
+		$this->assertEquals('d', $this->buffer[3]);
+		$this->assertEquals(4, count($this->buffer));
+ 		$this->assertEquals(-1, $reader->read($this->buffer, 0, 4));
+  	}
+ 	
+ 	public function testReadPartOfString() {
+ 		$reader = new StringReader('abcd');
+ 		$this->assertEquals(2, $reader->read($this->buffer, 0, 2));
+ 		$this->assertEquals('a', $this->buffer[0]);
+ 		$this->assertEquals('b', $this->buffer[1]);
+ 		$this->assertEquals(2, count($this->buffer));
+ 	}
+ 	
+ 	public function testReadWithOffsetPartOfString() {
+ 		$reader = new StringReader('abcd');
+ 		$this->assertEquals(4, $reader->read($this->buffer, 2, 4));
+ 		$this->assertFalse(array_key_exists(0, $this->buffer));
+ 		$this->assertFalse(array_key_exists(1, $this->buffer));
+ 		$this->assertEquals('a', $this->buffer[2]);
+ 		$this->assertEquals('b', $this->buffer[3]);
+ 	 }
+ 	
+ 	 public function testReadWithOffsetTooLargePartOfString() {
+ 	 	$reader = new StringReader('abcd');
+ 	 	$this->assertEquals(4, $reader->read($this->buffer, 6, 4));
+ 	 	$this->assertFalse(array_key_exists(0, $this->buffer));
+ 	 	$this->assertFalse(array_key_exists(1, $this->buffer));
+ 	 	$this->assertFalse(array_key_exists(2, $this->buffer));
+ 	 	$this->assertFalse(array_key_exists(3, $this->buffer));
+ 	 }
+ 	
+ 	public function testReadUntilEof() {
+ 		$reader = new StringReader('abcd');
+ 		$this->assertEquals(2, $reader->read($this->buffer, 0, 2));
+ 		$this->assertEquals('a', $this->buffer[0]);
+ 		$this->assertEquals('b', $this->buffer[1]);
+ 	
+ 		$this->assertEquals(2, $reader->read($this->buffer, 0, 3));
+ 		$this->assertEquals('c', $this->buffer[0]);
+ 		$this->assertEquals('d', $this->buffer[1]);
+ 	
+ 		$this->assertEquals(-1, $reader->read($this->buffer, 0, 2));
+ 	}
+ 	
+ 	 public function testReadWithUnicode() {
+ 	 	$reader = new StringReader('ðinæ');
+ 	 	$this->assertEquals(4, $reader->read($this->buffer, 0, 4));
+ 	 	$this->assertEquals('ð', $this->buffer[0]);
+ 	 	$this->assertEquals('i', $this->buffer[1]);
+ 	 	$this->assertEquals('n', $this->buffer[2]);
+ 	 	$this->assertEquals('æ', $this->buffer[3]);
+ 	 	$this->assertEquals(4, count($this->buffer));
+ 	 }
+ 	
+ 	 public function testReadWithUnicodePartOfString() {
+ 	 	$reader = new StringReader('ðinæ');
+ 	 	$this->assertEquals(2, $reader->read($this->buffer, 0, 2));
+ 	 	$this->assertEquals('ð', $this->buffer[0]);
+ 	 	$this->assertEquals('i', $this->buffer[1]);
+ 	 	$this->assertEquals(2, count($this->buffer));
+ 	 }
+ 	
+ 	 public function testReadWithUnicodeAndOffsetPartOfString() {
+ 	 	$reader = new StringReader('ðinæ');
+ 	 	$this->assertEquals(4, $reader->read($this->buffer, 2, 4));
+ 	 	$this->assertFalse(array_key_exists(0, $this->buffer));
+ 	 	$this->assertFalse(array_key_exists(1, $this->buffer));
+ 	 	$this->assertEquals('ð', $this->buffer[2]);
+ 	 	$this->assertEquals('i', $this->buffer[3]);
+ 	 }
+ 	
+ 	 public function testReadWithUnicodeAndOffsetTooLargePartOfString() {
+ 	 	$reader = new StringReader('ðinæ');
+ 	 	$this->assertEquals(4, $reader->read($this->buffer, 6, 4));
+ 	 	$this->assertFalse(array_key_exists(0, $this->buffer));
+ 	 	$this->assertFalse(array_key_exists(1, $this->buffer));
+ 	 	$this->assertFalse(array_key_exists(2, $this->buffer));
+ 	 	$this->assertFalse(array_key_exists(3, $this->buffer));
+ 	 }
+ 	
+ 	 public function testReadWithUnicodeUntilEof() {
+ 	 	$reader = new StringReader('ðinæ');
+ 	 	$this->assertEquals(3, $reader->read($this->buffer, 0, 3));
+ 	 	$this->assertEquals('ð', $this->buffer[0]);
+ 	 	$this->assertEquals('i', $this->buffer[1]);
+ 		
+ 	 	$this->assertEquals(1, $reader->read($this->buffer, 0, 3));
+ 	 	$this->assertEquals('æ', $this->buffer[0]);
+ 		
+ 	 	$this->assertEquals(-1, $reader->read($this->buffer, 0, 2));
+ 	 }
+	
+}
